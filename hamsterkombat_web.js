6dc0d759d37c8717517d5c4f237d4a0fe56fa3a8
Gerfesto <37264286+GravelFire@users.noreply.github.com>
2024-07-04 00:47:09 +0300

    Create hamsterkombat_web.js

---
+++ hamsterkombat_web.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         ХомякВеб
+// @namespace    https://hamsterkombat.io/*
+// @version      1.0
+// @description  Ну что тут написать, запускает хомяка в WEB
+// @author       VladimirSauko
+// @include      *://hamsterkombat.io/*
+// @icon         https://hamsterkombat.io/images/icons/hamster-coin.png
+// @downloadURL  https://github.com/GravelFire/HamsterKombatWebJS/raw/master/hamsterkombat_web.js
+// @updateURL    https://github.com/GravelFire/HamsterKombatWebJS/raw/master/hamsterkombat_web.js
+// @homepage     https://github.com/GravelFire/HamsterKombatWebJS
+// ==/UserScript==
+//
+(function() {
+    'use strict';
+
+const customConsole = {log: function() {}};
+unsafeWindow.console = customConsole;
+window.console = customConsole;
+
+if (unsafeWindow && unsafeWindow.Telegram && unsafeWindow.Telegram.WebApp)
+    unsafeWindow.Telegram.WebApp = new Proxy(unsafeWindow.Telegram.WebApp, {
+        get(target, prop) {
+            if (prop === 'platform')
+                return 'android';
+            if (typeof target[prop] === 'function')
+                return function(...args) {
+                    if (prop === 'exampleMethod') args[0] = 'modified argument';
+                    return target[prop].apply(this, args);
+                };
+            return target[prop];
+        }
+    });
+})();
